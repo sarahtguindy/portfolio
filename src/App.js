005133677c5e85aa56a5f9e73ba44a8c029b04bc
Sarah Guindy <sarahtg98@gmail.com>
2022-03-07 12:38:12 -0500

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Console from "./components/Console";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Footer />
+      <Console />
     </div>
   );
 };
