98c52d59cf0ca7828ea7cc451d306194730a1717
Sarah Guindy <sarahtguindy@gmail.com>
2023-04-15 16:51:47 -0400

    Remove Skills component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import About from "./components/About/About";
+import Console from "./components/Console/Console";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <About />
+      <Console />
       <Projects />
       <Contact />
       <Footer />
